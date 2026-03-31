b2377a6a76f0bc1a42654c74560bdbd7309fccc8
Hhyunseung <105931128+Hhyunseung@users.noreply.github.com>
2026-03-31 11:26:26 +0900

    진자운동 장애물
    m_OneMove 체크 시 한번만 움직임
    방향 바꾸려면 m_MaxAngle 에 부호 붙이면됨

--- GameClient/Source/Scripts/CPendulumObstacle.cpp
+++ GameClient/Source/Scripts/CPendulumObstacle.cpp
@@ -6,11 +6,15 @@
 
 CPendulumObstacle::CPendulumObstacle()
 	: CObstructScript(SCRIPT_TYPE::PENDULUMOBSTACLE)
-	, m_InitialPos(100.f, 0.f, 0.f)
+	, m_PivotPos(0.f, 0.f, 0.f)
+	, m_PivotToCenter(0.f, 200.f, 0.f)
 	, m_AccTime(0.f)
 	, m_MaxAngle(30.f) // 최대 각도 (예시: 30도)
 	, m_Frequency(1.f) // 진동 주파수 (예시: 1Hz)
-	, m_PendulumLength(100.f) // 진자의 길이 (예시: 100 units)
+	, m_PendulumLength(100.f) 
+	, m_OneMove(false)
+	, m_IsStopped(false)
+	, m_OneMoveTime(0.5f)
 {
 }
 
@@ -23,45 +27,75 @@ void CPendulumObstacle::Init()
 {
 	CObstructScript::Init(); // 부모 클래스의 Init() 호출
 
-	AddScriptParam(SCRIPT_PARAM::VEC4, &m_InitialPos, L"InitialPos", true, 0.f);
-	AddScriptParam(SCRIPT_PARAM::FLOAT, &m_AccTime, L"AccTime", true, 0.f);
+	AddScriptParam(SCRIPT_PARAM::VEC4, &m_PivotToCenter, L"PivotToCenter", true, 0.f);
 	AddScriptParam(SCRIPT_PARAM::FLOAT, &m_MaxAngle, L"MaxAngle", true, 0.f);
 	AddScriptParam(SCRIPT_PARAM::FLOAT, &m_Frequency, L"Frequency", true, 0.f);
 	AddScriptParam(SCRIPT_PARAM::FLOAT, &m_PendulumLength, L"PendulumLength", true, 0.f);
+	AddScriptParam(SCRIPT_PARAM::BOOL, &m_OneMove, L"OneMove", true, 0.f);
+	AddScriptParam(SCRIPT_PARAM::FLOAT, &m_OneMoveTime, L"OneMoveTime", true, 0.f);
 
-
+	Vec3 startPos = GetOwner()->Transform()->GetRelativePos();
+	m_PivotPos = startPos; // 회전 중심 위치는 초기 위치를 기준으로 설정
+	m_PivotPos += m_PivotToCenter;
 }
 
 void CPendulumObstacle::ApplySpawnInfo(const FSpawnInfo& info)
 {
+
 }
 
 
 void CPendulumObstacle::Move()
 {
+	if (m_OneMove && m_IsStopped)
+		return;
+
 	m_AccTime += DT;
 
-	// 최대 각도(degree -> radian)
+	// (degree -> radian) 
 	float maxAngleRad = XMConvertToRadians(m_MaxAngle);
+	float currentAngle = 0.f;
 
-	// 현재 각도
-	float currentAngle = maxAngleRad * sinf(XM_2PI * m_Frequency * m_AccTime);
+	if (m_OneMove)
+	{
+		float t = m_AccTime / m_OneMoveTime; 
 
-	// 진자의 회전 중심에서 현재 각도에 따른 위치 계산
-	Vec3 pivotPos = m_InitialPos;
-	pivotPos.y += m_PendulumLength;
+		if (t >= 1.f)
+		{
+			t = 1.f;
+			m_IsStopped = true; // 움직임 멈춤
+		}
 
-	Vec3 pos;
-	pos.x = pivotPos.x + m_PendulumLength * sinf(currentAngle);
-	pos.y = pivotPos.y - m_PendulumLength * cosf(currentAngle);
-	pos.z = m_InitialPos.z;
+		currentAngle = maxAngleRad + (2.f * -maxAngleRad * t);
+	}
+	else
+	{
+		// sin (2 * PI * frequency * time) 사인 그래프 ... maxAngleRad 범위 내에서 왕복하게 된다
+		currentAngle = maxAngleRad * sinf(XM_2PI * m_Frequency * m_AccTime);
+	}
 
+
+	// 피봇에서 중심까지의 벡터 // O -----> P 에서 P -----> O로 바꿔준다 (회전 방향이 반대이므로)
+	Vec3 centerOffset = -m_PivotToCenter;
+
+
+	float c = cosf(currentAngle);
+	float s = sinf(currentAngle);
+	
+	// 2D 회전 공식
+	// x' = x cosθ - y sinθ
+	// y' = x sinθ + y cosθ
+	Vec3 rotatedOffset;
+	rotatedOffset.x = centerOffset.x * c - centerOffset.y * s;
+	rotatedOffset.y = centerOffset.x * s + centerOffset.y * c;
+	rotatedOffset.z = centerOffset.z;
+
+	// 회전된 위치 = 피봇 위치 + 회전된 오프셋
+	Vec3 pos = m_PivotPos + rotatedOffset;
 	GetOwner()->Transform()->SetRelativePos(pos);
 
-
-	// 오브젝트 회전
-	Vec3 rot = Transform()->GetRelativeRot();
-	rot.z = currentAngle; // Z축 회전 적용
+	Vec3 rot = GetOwner()->Transform()->GetRelativeRot();
+	rot.z = currentAngle; // Z-axis rotation
 	GetOwner()->Transform()->SetRelativeRot(rot);
 }
 
--- GameClient/Source/Scripts/CPendulumObstacle.h
+++ GameClient/Source/Scripts/CPendulumObstacle.h
@@ -5,13 +5,17 @@ class CPendulumObstacle :
     public CObstructScript
 {
 private:
-	Vec3    m_PivotPos; // 진자의 회전 중심 위치
+	Vec3    m_PivotPos;
+	Vec3    m_PivotToCenter; 
 	
 	float   m_AccTime; // 누적 시간 (진동 계산용)
 	float   m_MaxAngle; // 진자의 최대 회전 각도 
 	float   m_Frequency; // 진자의 진동 주파수
     float   m_PendulumLength; // 진자의 길이
 
+    bool    m_OneMove; // 한번만 움직일거면 ture
+    bool    m_IsStopped;
+	float   m_OneMoveTime; // 한번만 움직이는 경우, 움직인 시간 기록
 
 public:
     virtual void Init() override;
