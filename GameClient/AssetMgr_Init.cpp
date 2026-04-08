94269daa94df7d4ca251dcd422ebcda8a5d6ecf9
Hhyunseung <105931128+Hhyunseung@users.noreply.github.com>
2026-04-08 12:33:09 +0900

    AlphaShader 추가

--- GameClient/AssetMgr_Init.cpp
+++ GameClient/AssetMgr_Init.cpp
@@ -245,6 +245,19 @@ void AssetMgr::CreateEngineShader()
 
 	AssetMgr::GetInst()->AddAsset(pShader->GetName(), pShader.Get());
 
+	// ============== AlphaShader 持失 ==============//
+	pShader = new AGraphicShader;
+	pShader->SetName(L"AlphaShader");
+	pShader->CreateVertexShader(L"Shader\\std2d.fx", "VS_Std2D");
+	pShader->CreatePixelShader(L"Shader\\std2d.fx", "PS_Std2D");
+	pShader->SetBSType(BS_TYPE::ALPHABLEND);
+	pShader->SetRSType(RS_TYPE::CULL_NONE);
+	pShader->SetDSType(DS_TYPE::NO_TEST_NO_WRITE);
+
+	pShader->AddShaderParam(SHADER_PARAM::VEC4, 0, L"TintColor");
+
+	AddAsset(L"AlphaShader", pShader.Get());
+
 	// ============== HPBarShader 持失 ==============//
 	pShader = new AGraphicShader;
 	pShader->SetName(L"ButtonShader");
@@ -473,6 +486,18 @@ void AssetMgr::CreateEngineMaterial()
 	pMtrl->SetTexture(TEX_0, FindAsset<ATexture>(L"TileShipStage1_2"));
 
 	pMtrl->SetDomain(RENDER_DOMAIN::DOMAIN_OPAQUE);
+	AddAsset(pMtrl->GetName(), pMtrl.Get());
+
+
+	// ================== AlphaMtrl 持失 ==============//
+	pMtrl = new AMaterial;
+	pMtrl->SetName(L"UI_AlphaPanel");
+	pMtrl->SetShader(FindAsset<AGraphicShader>(L"AlphaShader"));
+
+	pMtrl->SetScalar(VEC4_0, Vec4(1.f, 1.f, 1.f, 1.f));
+	pMtrl->SetTexture(TEX_0, FindAsset<ATexture>(L"UI_ingame_ovenBreakGauge"));
+
+	pMtrl->SetDomain(RENDER_DOMAIN::DOMAIN_TRANSPARENT);
 	AddAsset(pMtrl->GetName(), pMtrl.Get());
 
 	// ================== UIButtonMtrl 持失 ==============//
